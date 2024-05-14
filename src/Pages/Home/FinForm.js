dc9d270eb11b65b36a6063aeb03a61f1f9911351
joshua <jozzydgreat1@gmail.com>
2024-05-13 20:22:21 -0700

    ejuandy first commit

---
+++ src/Pages/Home/FinForm.js
@@ -0,0 +1,9 @@
+import { Forms } from './Forms'
+import {Navbar} from './Navbar'
+export const FinForm = ()=>{
+    return(
+        <div>
+           <Forms/>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { AuthContextProvider } from './Context/AuthContext/AuthContextProvider';
+import { IpContext } from './Context/IpCon/IpContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    <AuthContextProvider>
+      <IpContext>
+      <App />
+      </IpContext>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
