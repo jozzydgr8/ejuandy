f1af1b43c337fa47920d2d68dbb00da818292db8
joshua <jozzydgreat1@gmail.com>
2024-05-15 04:42:05 -0700

    added blog link to footer

--- src/Pages/Home/Footer.js
+++ src/Pages/Home/Footer.js
@@ -4,6 +4,7 @@ export const Footer = ()=>{
         <>
             <section id='footer-section'>
                 <div className='container-fluid background'>
+                <a className='footer-section-link' href='http://www.gistmetvmag.blogspot.com/' target='_blank' >visit our blog !</a>
                 <h4><a className="footer-section-link" href="https://instagram.com/gistmetvmag?igshid=NTc4MTIwNjQ2YQ==" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a> <a className="footer-section-link" href="https://www.facebook.com/GistMeTvMag" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a> <a className="footer-section-link" target="_blank" href="https://www.youtube.com/@YobaMovies"><ion-icon name="logo-youtube"></ion-icon></a></h4>
                    <h6 className="monserrat">© copyright ejuandy {currentYear}</h6>
                 </div>
